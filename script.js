49c6649695cb9c6ab84e50d84b7c0988db260b4e
Eliaz <randreliaz@protomail.com>
2026-05-05 12:19:31 +0300

    modifie

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 <script>
-   let display = document.getElementById("display");
-   function add(value)
-   {
-     display.value += value 
-    
+let display = document.getElementById("display");
+
+function add(value) {
+    display.value += value;
+}
+
+function del() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = "Error";
     }
-     function calculate()
-   {
-     display.value = eval(display.value)
-    }
-     function del()
-   {
-     display.value =""
-    }
+}
 </script>
